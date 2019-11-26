5b81e2b098dc72af117cf3d398ccc1e4f035184a
Engineering Manager <newsoftmalls@gmail.com>
2019-11-26 13:49:01 +0200

    components commit

--- reactdjangoreduxapp/frontend/src/components/App.js
+++ reactdjangoreduxapp/frontend/src/components/App.js
@@ -1,24 +1,27 @@
 /**
- * KYIV MEDIA 25.11.2019
-*/
-import ReactDom from 'react-dom';
-import React, { Component } from 'react';
-import Header from './layout/Header';
+ * KYIV MEDIA 26.11.2019
+ */
+import ReactDom from "react-dom";
+import React, { Component, Fragment } from "react";
+import Header from "./layout/Header";
+import Dashboard from "./leads/Dashboard";
 
 export default class App extends Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props);
 
-        this.state = {
-                 
-        }
-    }
+    this.state = {};
+  }
 
-    render() {
-        return (
-            <Header />
-        )
-    }
+  render() {
+    return (
+      <Fragment>
+        <Header />
+        <div className="container">
+          <Dashboard />
+        </div>
+      </Fragment>
+    );
+  }
 }
-ReactDom.render(<App />,document.getElementById('app'));
-
+ReactDom.render(<App />, document.getElementById("app"));
---
+++ reactdjangoreduxapp/frontend/src/components/leads/Leads.js
@@ -0,0 +1,16 @@
+/**
+ * KYIV MEDIA 26.11.2019
+ */
+import React, { Component } from "react";
+
+export class Leads extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Leads List</h1>
+      </div>
+    );
+  }
+}
+
+export default Leads;
